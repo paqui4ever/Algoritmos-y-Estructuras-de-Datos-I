da331beb6a944c19192f0d65cb7ec4033ed0a7ad
paqui4ever <s.rene.picard@gmail.com>
2024-04-18 11:53:27 -0300

    Hice primeros 2 ejemplos del video 5 simple steps for solving any recursive problem en el archivo de la teorica de recursion

--- guia4/recursionteorica.hs
+++ guia4/recursionteorica.hs
@@ -2,3 +2,16 @@ f :: Integer -> Integer
 
 f n | n == 1 = 1 -- Defino el caso base en el cual f(1)=1
     | otherwise = f (n-1) + 2*n - 1 --Defino f(n) en base a f(n-1)
+
+
+suma :: Integer -> Integer -- Dado un n, sumar todos los n positivos hasta ese n (menor estricto)
+suma 0 = 0
+suma x = suma(x-1) + (x-1)
+
+suma2 :: Integer -> Integer --Lo mismo que el de arriba pero en la suma es hasta n inclusive
+suma2 0 = 0 
+suma2 x = suma2(x-1) + x
+
+numberOfPaths :: Integer -> Integer -> Integer --Dado un grid de n x m, dar el número de paths posibles para llegar desde arriba a la izquierda hasta abajo a la derecha con solo movimientos para abajo y a la derecha
+numberOfPaths n m | n == 1 || m == 1 = 1
+                  | otherwise = (numberOfPaths (n-1) m) + (numberOfPaths n (m-1))
