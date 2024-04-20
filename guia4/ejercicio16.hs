33b78dbebe9f66a0456b8c910b7bcbfc0bb37e3a
paqui4ever <s.rene.picard@gmail.com>
2024-04-20 19:54:21 -0300

    Hice punto b) del ejercicio 16 de la guia 4, y dejé a medias el 16) c), el cual sigo mañana

--- guia4/ejercicio16.hs
+++ guia4/ejercicio16.hs
@@ -1,3 +1,5 @@
+-- Punto a)
+
 menorDivisor :: Integer -> Integer
 
 menorDivisor x | x == 1 = 1
@@ -7,3 +9,23 @@ menorDivisorDesde :: Integer -> Integer -> Integer
 
 menorDivisorDesde x y | mod x y == 0 = y
                       | otherwise = menorDivisorDesde x (y+1)
+
+-- Punto b) 
+
+esPrimo :: Integer -> Bool
+
+esPrimo x | x <= 0 = False
+          | otherwise = mod x x == 0 && mod x 1 == 0
+
+-- Punto c), FALTA TERMINAR
+
+sonCoprimos :: Integer -> Integer -> Bool
+
+sonCoprimos x y | x < 1 || y < 1 = False
+                | x == 1 || y == 1 = True 
+                | otherwise = 
+
+sonMultiplos :: Integer -> Integer -> Bool
+
+sonMultiplos x y | x == y = True
+                 | otherwise = mod x y == 0
