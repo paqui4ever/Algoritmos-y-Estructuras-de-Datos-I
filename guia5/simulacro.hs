a93a31061d7780f6c7827ee1dd0c63d167ce2270
paqui4ever <s.rene.picard@gmail.com>
2024-04-25 12:47:47 -0300

    Terminé la funcion amigosDe del simulacro

--- guia5/simulacro.hs
+++ guia5/simulacro.hs
@@ -14,7 +14,7 @@ personas [] = ["Nadie"]
 personas [(x,y)] = [x,y]
 personas (x:y:xs) = quitarIguales(fst x : snd x : personas (y:xs))
 
-quitarIguales :: [String] -> [String] -- Como tendria que haber hecho la funcion en el simulacro
+quitarIguales :: [String] -> [String] -- Como tendria que haber hecho la funcion en el simulacro asi me quedaban todos nombres unicos en el output de la funcion personas
 quitarIguales [] = []
 quitarIguales (x:xs) | elementoEnLaLista x xs = quitarIguales xs  -- Podria haber usado la funcion incluida en haskell elem pero la hago yo por las dudas
                      | otherwise = x: quitarIguales xs
@@ -27,8 +27,29 @@ elementoEnLaLista n (x:xs) | n == x = True
 extraerElementosDeTuplas :: (String, String) -> [String] -- Funcion que no terminé usando pero la dejo acá porque está buena
 extraerElementosDeTuplas (x,y) = fst (x,y) : [snd (x,y)]
 
-amigosDe :: String -> [(String, String)] -> [String]
-amigosDe "Nadie" [] = ["Nadie"]
+amigosDeV1 :: String -> [(String, String)] -> [String] -- Version inicial de la función, no funciona del todo bien
+amigosDe _ [] = []
+amigosDe "Nadie" [x] = ["Nadie"]
+amigosDe n (x:xs) | elementoEnLaLista n (extraerElementosDeTuplas x) == True = quitar n (extraerElementosDeTuplas x)
+                  | otherwise = amigosDe n xs
+
+amigosDeV2 :: String -> [(String, String)] -> [String] -- Version definitiva de la funcion amigosDe
+amigosDeV2 _ [] = []
+amigosDeV2 n ((x,y):xs) | (n == x || n == y) = quitarElementoDeTupla n (x,y) : amigosDeV2 n xs
+                        | otherwise = amigosDeV2 n xs
+
+-- amigosDe Silvano [("Silvano", "Martina"), ("Pedro", "Silvano"), ("Tomas", "Pepe"), ("Silvano", "Pepe"), ("Juan", "Pedro")] = ["Martina", "Pedro", "Pepe"]
+-- amigosDe Silvano [("Silvano", "Martina"), ("Pedro", "Juan")] => ["Silvano", "Martina", "Pedro", "Juan"]
+-- Extraigo elementos de las tuplas y saco los pares que estan junto al n que busco
+ 
+
+quitarElementoDeTupla :: String -> (String, String) -> String
+quitarElementoDeTupla n (x,y) | n == x = y
+                              | otherwise = x
+
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar y (x:xs) | y == x = xs
+                | otherwise = x : quitar y xs 
 
 --personaConMasAmigos :: [(String, String)] -> String
 
