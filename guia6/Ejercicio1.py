5ba0a8b57c5b3c5a531cbabc6a23234c44afad72
paqui4ever <s.rene.picard@gmail.com>
2024-05-19 15:30:38 -0300

    Guia 6: Ejercicio 1 agregado

---
+++ guia6/Ejercicio1.py
@@ -0,0 +1,30 @@
+import math 
+
+# Ejercicio 1 
+def imprimir_hola_mundo ():
+    print ("¡Hola mundo!")
+
+# Ejercicio 2
+def imprimir_un_verso ():
+    print ("Now here I go again \nI see the crystal visions \nI keep my visions to myself \nIt's only me who wants to wrap around your dreams \nAnd have you any dreams you'd like to sell? \nDreams of loneliness")
+
+# Ejercicio 3
+
+def raizDe2 ():
+    print (round (math.sqrt (2), 4))
+
+raizDe2()
+
+# Ejercicio 4
+
+def factorial_de_dos ():
+    print (math.factorial (2))
+    
+factorial_de_dos ()
+
+# Ejercicio 5
+
+def perimetro ():
+    print (math.pi * 2)
+    
+perimetro ()
