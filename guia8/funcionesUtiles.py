e6fd77d6d383cf3b8c92f4bd4ad960a30be1be81
paqui4ever <s.rene.picard@gmail.com>
2024-06-04 12:44:41 -0300

    Guia 8: Corrección en funcion split()

--- guia8/funcionesUtiles.py
+++ guia8/funcionesUtiles.py
@@ -4,18 +4,39 @@ def pertenece (s, lista: list) -> bool:
             return True
     return False
 
-def split_homemade(frase: str) -> str:
-    string: str = []
-    for caracter in frase:
-        if caracter != " ":
-            string.append(caracter)
-    return pasar_lista_a_string(string)
-
 def pasar_lista_a_string (lista: list[chr]) -> str:
     frase: str = ""
     for i in range (len(lista)):
         frase += lista[i]
     return frase
 
-#print(split_homemade("Hola Como andas"))
+def loopear (string: str):
+    for palabra in string:
+        print(palabra)
+        
+#loopear("Hola Como")
+
+def split (string: str) -> list:
+    palabras = []
+    palabra = "" # Defino un string vacio para luego poder agregarlo a la lista palabras
+    i = 0 # Defino la variable a partir de la cual voy a contar
+    termino_palabra: bool = None # Me dice si estoy dentro de una palabra o no
+    while i < len(string):
+        if string[i] == " " or string[i] == "\n":
+            i += 1
+        else:
+            while i != len(string) and string[i] != " " and string[i] != "\n": # Agrego la palabra
+                palabra += string[i]
+                i += 1
+                termino_palabra = True  
+        
+        if termino_palabra == True: # Reseteo termino_palabra y agrego la palabra completa al resultado
+            palabras.append(palabra)
+            termino_palabra = False
+            palabra = ""
+
+    return palabras
+            
+#print(split("Tomas Juan"))
+
             
